b4aaf6881665ab94b20dca409bf1d081a23705cb
Pedro Junior <pedrotark@gmail.com>
2021-12-16 19:40:50 -0300

    Conceitos conjutos done

--- curso_python/coleçoes_python/conjuntos.py
+++ curso_python/coleçoes_python/conjuntos.py
@@ -34,6 +34,38 @@ ss = {1, 2, 3, 4, 5, 6, 6}
 #set(lista)
 #podemos saber a quantidade de elementos unicos combinando set e len
 
+#lembrar que sets/conjuntos nao possuem valor repetido
 
+#REMOVER ELEMENTOS DO CONJUNTO/SET
+#apontamos o valor do elemento a ser removido
+ss.remove(1) # não é o indice/posição, pois conjuntos nao sao indexados
 
+#segunda forma
+ss.discard(2)
 
+##COPIANDO DEEP COPPY
+novo = s.copy()
+
+##COPIANDO SHALLOW COPPy
+novo1 = ss
+
+##REMOVENDO TODOS OS ITENS COM CLEAR()
+novo.clear()
+
+#Metodos matematicos dos conjuntos
+
+estudantes_python = {'Marcos', 'Patricia', 'Ellen', 'Pedro', 'Julia', 'Guilheme'}
+estudantes_java = {'Fernando', 'Gustavo', 'Julia', 'Ana', 'Patricia'}
+
+#conjunto concatenar conjuntos
+#utilizando UNION()
+## Segunda forma atravez do | Exemplo: conjunto3 = estudantes_python | estudantes_java
+estudantes_dos_dois_cursos = estudantes_python.union(estudantes_java)
+
+##Forma de criar um conjunto somente com os elementos em comum das listas
+##Atravez do intersection
+ambos = estudantes_python.intersection(estudantes_java)
+
+##apenas os valores que nao estao em ambos conjuntos
+##utilizando .diference()
+diferença_estudantes = estudantes_python.difference(estudantes_java)
