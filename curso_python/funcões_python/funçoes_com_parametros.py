8208ee4f6fddf12ce95e652567ef9e703dd8e1cf
Pedro Junior <pedrotark@gmail.com>
2022-01-31 19:02:56 -0300

    Funçoes em python com parametros e parametros default

---
+++ curso_python/funcões_python/funçoes_com_parametros.py
@@ -0,0 +1,15 @@
+"""
+Funçoes com parametros de entrada
+
+Sem misterios, recebem dados para serem processados
+
+Parametro definidos são os parametros que sao instanciados com um valor,
+é necessario adicionar estes por ultimo como parametros
+
+varivaveis locais sobreepoem as globais, se puder evite var globais
+
+podemos apontar para global com a declaração de global antes de var
+
+
+
+"""
