be927e17ab4a0b9bca9c2020b7be587044281aba
Pedro Junior <pedrotark@gmail.com>
2022-03-11 05:20:55 -0300

    Seek e cursor

--- curso_python/leitura_e_escrita/leitura_de_arquivos.py
+++ curso_python/leitura_e_escrita/leitura_de_arquivos.py
@@ -12,3 +12,19 @@
 
 
 """
+
+#Este abre o arquivo, é necessarioo outro metodo para leitura
+arquivo = open('texto.txt')
+
+##Mostrara as especificações do arquivo name, mode, encoding. class _io.TextIOWrapper
+## Mode= 'r' = somerte leitura -> read()
+# encounding = 'UTF-8'
+print(arquivo)
+
+##Apos abrir o arquivo, este metodo le o arquivo
+## Metodo de leitura funciona a partir de um cursor que começa no
+## Inicio do texto e ao terminar leva o cursor para o final do texto
+
+## READ() LE TODO CONTEUDO DO ARQUIVO PROFESSOR LOOUCO
+print(arquivo.read())
+
