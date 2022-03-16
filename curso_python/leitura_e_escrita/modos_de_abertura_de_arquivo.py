7ee408b8540343133e857a16e0f97853c647bcda
Pedro Junior <pedrotark@gmail.com>
2022-03-16 05:39:55 -0300

    Modos de abertura de arquivo

---
+++ curso_python/leitura_e_escrita/modos_de_abertura_de_arquivo.py
@@ -0,0 +1,11 @@
+"""
+    Modos de abertura de arquivo
+
+    r -> para leitura - padrão
+    w -> para escrita - e sobrescreve o arquivo caso ja exista
+    x -> para escrita somente se o arq nao existir
+    a -> abre para a escrita concatenando o conteudo ao final do arquivo
+    OBS: abrindo no a de "append" caso nao exista cria oa arq tbm
+    + -> Abre para o modo de atualização leitura e escrita
+    obs: R e W mantemos o controle do cursor, "A" não permite
+"""
