04ac971d4918666ec4ae132ab68ab83b2001b58d
Pedro Junior <pedrotark@gmail.com>
2022-03-31 04:55:32 -0300

    testes de memoria por generetors

---
+++ curso_python/Iteradores_e_iteraveis/testes_de_memoria_com_generetors.py
@@ -0,0 +1,21 @@
+"""
+    Teste de Memoria usando Generators
+
+"""
+
+#Por lista 500mb
+def fib_lista(max):
+    nums = []
+    a, b = 0, 1
+    while len(nums) < max:
+        nums.append(b)
+        a, b = b, a + b
+    return nums
+
+#Função usando geradores 3mb
+def fib_gen(max):
+    a, b, contador = 0, 1, 0
+    while contador < max:
+        a, b = b, a + b
+        yield a
+        contador = contador + 1
