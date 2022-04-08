48153bcf78dddc4eb7a10dcff8b4f7e58ee1fb45
Pedro Junior <pedrotark@gmail.com>
2022-04-08 04:53:09 -0300

    POO - Classes

---
+++ curso_python/programação_orientada_objeto/classes.py
@@ -0,0 +1,18 @@
+"""
+    Classes - Modelos de Objeto
+
+    Contem:
+        Adtributos - estados de um objeto, caracteristicas, informaçoes
+        Metodos - Comportamentos, rotinas de processo, açoes,
+        Definir classe - atravez do tipo class Classe com letra maiscula convençao
+            OBS: classes internas e primitivas sao com letra minuscula
+            OBS: sem acentuação, espaços ou caracteres especiais
+
+    No mapeamento logico na hora do planejamento objetos podem ser chamados de entidades.
+"""
+class Lampada:
+    pass
+
+item = Lampada();
+
+print(type(item))
