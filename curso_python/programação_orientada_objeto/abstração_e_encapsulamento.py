f5518a20ba0146f98368cf297d79219e6c7607ef
Pedro Junior <pedrotark@gmail.com>
2022-04-12 18:38:23 -0300

    Abstração e Encapsulamento

---
+++ curso_python/programação_orientada_objeto/abstração_e_encapsulamento.py
@@ -0,0 +1,11 @@
+"""
+    POO - Abstração e encapsulamento
+
+    Paradigma que pensamos na forma menos trabalhosa de resolver os problemas
+        -> Proposito do POO Encapsular de forma logica e hierarquica utilizando classes
+
+    Encapsular -> Encapsular elementos de uma classe
+    Abstração -> Mostrar somente o necessario com metodos privados de cada classe __
+
+
+"""
