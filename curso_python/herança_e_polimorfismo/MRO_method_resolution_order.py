24fa6d930aa49c1479b3d691549a868c5cad1330
Pedro Junior <pedrotark@gmail.com>
2022-04-13 20:21:05 -0300

    MRO - Method Resolution Order

---
+++ curso_python/herança_e_polimorfismo/MRO_method_resolution_order.py
@@ -0,0 +1,12 @@
+"""
+    MRO - Method Resolution Order
+
+    -> Ordem de qual metodo sera executado primeiro
+
+    -> Podemos conferir esta ordem de e formas
+        -> via propriedade da classe __NomeDaClasse/Arquivo__(nome da classe)
+        -> via metodo NomeDaClasse/Arquiv()
+        -> via help
+"""
+#Ex: Classe.__NomeDoArquivo__
+#Ex2: Abrindo o terminal e digitando help(Classe)
