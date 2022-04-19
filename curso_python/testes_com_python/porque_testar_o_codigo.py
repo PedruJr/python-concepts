873356d69ae6b16c660b5b69829a8e3b9e463b45
Pedro Junior <pedrotark@gmail.com>
2022-04-19 08:39:19 -0300

    Informaçoes sobre testes com python

---
+++ curso_python/testes_com_python/porque_testar_o_codigo.py
@@ -0,0 +1,19 @@
+"""
+    Porque testar nosso codigo?
+    -> Reduzir bugs.
+    -> Novos recursos não impactam nos recursos antigos.
+    -> Testar as refatorações de codigo, cuidando caso haja bugs.
+
+    O que o teste faz?
+    -> Teste verifica se o processo esta entregando a saida desejada
+
+    -> Teste Manual
+    -> Teste Automatizado
+
+    TDD -> Testen Driven Development (Desenvolvimento Guiado Por Testes)
+    -> Funciona por etapas, Primeiro escreve os testes, depois os testes.
+    -> Red; -> Teste falha
+    -> Green; -> Teste passou
+    -> Refactor; -> Refatorar para funcionar corretamente.
+
+"""
