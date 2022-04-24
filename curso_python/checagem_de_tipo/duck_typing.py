b7e2d65786787a5f0d1851274a32ff5b340cfe38
Pedro Junior <pedrotark@gmail.com>
2022-04-24 08:13:43 -0300

    Duck Typing

---
+++ curso_python/checagem_de_tipo/duck_typing.py
@@ -0,0 +1,7 @@
+"""
+    Duck Typing
+    -> O tipo da variavel é menos importante que o conteudo dentro,
+    que é o que realmente define.
+    -> Objetos similares tem os mesmos comportamentos.
+
+"""
