5aeab25b0defb1d6e366eb292d1d84261bd3c203
Pedro Junior <pedrotark@gmail.com>
2022-04-24 09:36:46 -0300

    Duck Typing

--- curso_python/checagem_de_tipo/duck_typing.py
+++ curso_python/checagem_de_tipo/duck_typing.py
@@ -1,7 +1,23 @@
 """
-    Duck Typing
+    Duck Typing:
     -> O tipo da variavel é menos importante que o conteudo dentro,
     que é o que realmente define.
     -> Objetos similares tem os mesmos comportamentos.
 
+    Type Hinting:
+    -> Solução formal de formatação para apontar o tipo de dado
+
 """
+#Type Hinting:   #tipo de dado -> tipo de dado de retorno
+def cumprimentar(nome: str) -> str:
+    return f'Olá, {nome}'
+
+def cabecalho(texto: str, alinhamento: bool = True) -> str:
+    if alinhamento:
+        return f"{texto.title()}\n{'-' * len(texto)}"
+    else:
+        return f"{texto.title()}".center(50, '#')
+
+
+print(cabecalho('Geek University'))
+print(cabecalho('Geek University', alinhamento=False))
